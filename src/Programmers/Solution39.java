de6016b2e1f7624aee4e2ff0f1929fbed0aae21b
lolu <soowkw@naver.com>
2024-07-10 23:03:14 +0900

    프로그래머스 0단계

---
+++ src/Programmers/Solution39.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution39 {
+    public ArrayList<Integer> solution(int start_num, int end_num) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i = start_num; i >= end_num; i--) {
+            answer.add(i);
+        }
+        return answer;
+    }
+}
